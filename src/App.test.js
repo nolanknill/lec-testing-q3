fd095704be041455c27a2f500ad623e312fe88fc
Nolan Knill <nolanknill@gmail.com>
2022-09-19 10:49:20 -0400

    App component test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  it("should include a title", () => {
+    render(<App />);
+
+    expect(screen.getByText(/Lecture\: Testing/)).toBeInTheDocument();
+  })
 });
